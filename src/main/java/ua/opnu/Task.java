0b3fae7a36267312408246534d7c490ddd273c8b
Mykola <nick.godov@gmail.com>
2023-10-31 22:45:43 +0200

    tasks added

--- src/main/java/ua/opnu/Task.java
+++ src/main/java/ua/opnu/Task.java
@@ -1,7 +1,9 @@
 package ua.opnu;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 public class Task {
     public static void main(String[] args) {
@@ -41,6 +43,45 @@ public class Task {
 
     public void rearrange(Queue<Integer> queue) {
         // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+    }
+
+    public int maxLength(Set<String> set) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+    public void removeEvenLength(Set<String> set) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+    }
+
+    public int numInCommon(List<Integer> list1, List<Integer> list2) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+    public boolean isUnique(Map<String, String> map) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return false;
+    }
+
+    public Map<String, Integer> intersect(Map<String, Integer> map1, Map<String, Integer> map2) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return null;
+    }
+
+    public Map<String, Integer> reverse(Map<Integer, String> map) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return null;
+    }
+
+    public int rarest(Map<String, Integer> map) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+    public int maxOccurrences(List<Integer> list) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
     }
 
 }
--- src/test/java/ua/opnu/TaskTest.java
+++ src/test/java/ua/opnu/TaskTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +21,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the first task")
+    @DisplayName("Tests for the task 1")
     class Task1Test {
 
         @Test
@@ -84,7 +86,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the second task")
+    @DisplayName("Tests for the task 2")
     class Task2Test {
         @Test
         public void test1() {
@@ -154,7 +156,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the third task")
+    @DisplayName("Tests for the task 3")
     class Task3Test {
 
         @Test
@@ -232,7 +234,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the fourth task")
+    @DisplayName("Tests for the task 4")
     class Task4Test {
 
         @Test
@@ -298,7 +300,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the fifth task")
+    @DisplayName("Tests for the task 5")
     class Task5Test {
 
         @Test
@@ -364,7 +366,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the sixth task")
+    @DisplayName("Tests for the task 6")
     class Task6Test {
 
         @Test
@@ -479,7 +481,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the seventh task")
+    @DisplayName("Tests for the task 7")
     class Task7Test {
 
         @Test
@@ -497,7 +499,7 @@ public class TaskTest {
     }
 
     @Nested
-    @DisplayName("Tests for the eights task")
+    @DisplayName("Tests for the task 8")
     class Task8Test {
 
         @Test
@@ -582,6 +584,538 @@ public class TaskTest {
             task.rearrange(actual);
 
             assertArrayEquals(actual.toArray(new Integer[0]), expected.toArray(new Integer[0]));
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 9")
+    class Task9Test {
+
+        @Test
+        public void test1() {
+            Set<String> src = Set.of("tenletters");
+            int actual = task.maxLength(src);
+
+            assertEquals(10, actual);
+        }
+
+        @Test
+        public void test2() {
+            Set<String> src = Set.of("one", "two", "three", "four", "get your person", "on the floor", "gotta gotta get up to get", "down");
+            int actual = task.maxLength(src);
+
+            assertEquals(25, actual);
+        }
+
+        @Test
+        public void test3() {
+            Set<String> src = Collections.emptySet();
+            int actual = task.maxLength(src);
+
+            assertEquals(0, actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 10")
+    class Task10Test {
+        @Test
+        public void test1() {
+            Set<String> actual = Stream.of("foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude").collect(Collectors.toSet());
+            Set<String> expected = Stream.of("!", "bar", "foo", "spoon").collect(Collectors.toSet());
+            task.removeEvenLength(actual);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test2() {
+            Set<String> actual = Stream.of("foo", "bar").collect(Collectors.toSet());
+            Set<String> expected = Stream.of("foo", "bar").collect(Collectors.toSet());
+            task.removeEvenLength(actual);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test3() {
+            Set<String> actual = Collections.emptySet();
+            Set<String> expected = Collections.emptySet();
+            task.removeEvenLength(actual);
+
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 11")
+    class Task11Test {
+
+        @Test
+        public void test1() {
+            List<Integer> list1 = List.of(3, 7, 3, -1, 2, 3, 7, 2, 15, 15);
+            List<Integer> list2 = List.of(-5, 15, 2, -1, 7, 15, 36);
+            int actual = task.numInCommon(list1, list2);
+
+            assertEquals(4, actual);
+        }
+
+        @Test
+        public void test2() {
+            List<Integer> list1 = List.of(1, 2, 3);
+            List<Integer> list2 = List.of(3, 4, 5);
+            int actual = task.numInCommon(list1, list2);
+
+            assertEquals(1, actual);
+        }
+
+        @Test
+        public void test3() {
+            List<Integer> list1 = List.of(1, 2, 3);
+            List<Integer> list2 = List.of(2);
+            int actual = task.numInCommon(list1, list2);
+
+            assertEquals(1, actual);
+        }
+
+        @Test
+        public void test4() {
+            List<Integer> list1 = List.of(4);
+            List<Integer> list2 = List.of(1, 2, 3);
+            int actual = task.numInCommon(list1, list2);
+
+            assertEquals(0, actual);
+        }
+
+        @Test
+        public void test5() {
+            List<Integer> list1 = Collections.emptyList();
+            List<Integer> list2 = Collections.emptyList();
+            int actual = task.numInCommon(list1, list2);
+
+            assertEquals(0, actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 12")
+    class Task12Test {
+
+        @Test
+        public void test1() {
+            Map<String, String> map = new HashMap<>();
+            map.put("Marty", "Stepp");
+            map.put("Stuart", "Reges");
+            map.put("Jessica", "Miller");
+            map.put("Amanda", "Camp");
+            map.put("Hal", "Perkins");
+            boolean actual = task.isUnique(map);
+
+            assertTrue(actual);
+        }
+
+        @Test
+        public void test2() {
+            Map<String, String> map = new HashMap<>();
+            map.put("Kendrick", "Perkins");
+            map.put("Stuart", "Reges");
+            map.put("Jessica", "Miller");
+            map.put("Bruce", "Reges");
+            map.put("Hal", "Perkins");
+            boolean actual = task.isUnique(map);
+
+            assertFalse(actual);
+        }
+
+        @Test
+        public void test3() {
+            Map<String, String> map = new HashMap<>();
+            boolean actual = task.isUnique(map);
+
+            assertTrue(actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 13")
+    class Task13Test {
+
+        @Test
+        public void test1() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("Janet", 87);
+            map1.put("Logan", 62);
+            map1.put("Whitaker", 46);
+            map1.put("Alyssa", 100);
+            map1.put("Stefanie", 80);
+            map1.put("Jeff", 88);
+            map1.put("Kim", 52);
+            map1.put("Sylvia", 95);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("Logan", 62);
+            map2.put("Kim", 52);
+            map2.put("Whitaker", 52);
+            map2.put("Jeff", 88);
+            map2.put("Stefanie", 80);
+            map2.put("Brian", 60);
+            map2.put("Lisa", 83);
+            map2.put("Sylvia", 87);
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("Logan", 62);
+            expected.put("Stefanie", 80);
+            expected.put("Jeff", 88);
+            expected.put("Kim", 52);
+
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test2() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("a", 1);
+            map1.put("b", 2);
+            map1.put("c", 3);
+            map1.put("d", 4);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("b", 2);
+            map2.put("c", 5);
+            map2.put("d", 4);
+            map2.put("e", 4);
+            map2.put("f", 1);
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("b", 2);
+            expected.put("d", 4);
+
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test3() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("a", 1);
+            map1.put("b", 2);
+            map1.put("c", 3);
+            map1.put("d", 4);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("a", 1);
+            map2.put("b", 2);
+            map2.put("c", 3);
+            map2.put("d", 4);
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("a", 1);
+            expected.put("b", 2);
+            expected.put("c", 3);
+            expected.put("d", 4);
+
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test4() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("a", 1);
+            map1.put("b", 2);
+            map1.put("c", 3);
+            map1.put("d", 4);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("x", 0);
+            map2.put("a", 1);
+            map2.put("b", 2);
+            map2.put("c", 3);
+            map2.put("d", 4);
+            map2.put("e", 5);
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("a", 1);
+            expected.put("b", 2);
+            expected.put("c", 3);
+            expected.put("d", 4);
+
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test5() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("x", 0);
+            map1.put("a", 1);
+            map1.put("b", 2);
+            map1.put("c", 3);
+            map1.put("d", 4);
+            map1.put("e", 5);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("a", 1);
+            map2.put("b", 2);
+            map2.put("c", 3);
+            map2.put("d", 4);
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("a", 1);
+            expected.put("b", 2);
+            expected.put("c", 3);
+            expected.put("d", 4);
+
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test6() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("a", 1);
+            map1.put("b", 2);
+            map1.put("c", 3);
+            map1.put("d", 4);
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("a", 5);
+            map2.put("b", 6);
+            map2.put("c", 7);
+            map2.put("d", 8);
+
+            Map<String, Integer> expected = new HashMap<>();
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test7() {
+            Map<String, Integer> map1 = new HashMap<>();
+            map1.put("a", 1);
+            map1.put("b", 2);
+
+            Map<String, Integer> map2 = new HashMap<>();
+
+            Map<String, Integer> expected = new HashMap<>();
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test8() {
+            Map<String, Integer> map1 = new HashMap<>();
+
+            Map<String, Integer> map2 = new HashMap<>();
+            map2.put("a", 1);
+
+            Map<String, Integer> expected = new HashMap<>();
+            Map<String, Integer> actual = task.intersect(map1, map2);
+
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 14")
+    class Task14Test {
+
+        @Test
+        public void test1() {
+            Map<Integer, String> map = new TreeMap<>();
+            map.put(42, "Marty");
+            map.put(81, "Sue");
+            map.put(17, "Ed");
+            map.put(31, "Dave");
+            map.put(56, "Ed");
+            map.put(3, "Marty");
+            map.put(29, "Ed");
+
+            Map<String, Integer> expected = new TreeMap<>();
+            expected.put("Sue", 81);
+            expected.put("Marty", 42);
+            expected.put("Dave", 31);
+            expected.put("Ed", 56);
+
+            Map<String, Integer> actual = task.reverse(map);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test2() {
+            Map<Integer, String> map = new HashMap<>();
+            map.put(13, "King");
+            map.put(12, "Queen");
+            map.put(11, "Jack");
+            map.put(1, "Ace");
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("Ace", 1);
+            expected.put("King", 13);
+            expected.put("Queen", 12);
+            expected.put("Jack", 11);
+
+            Map<String, Integer> actual = task.reverse(map);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test3() {
+            Map<Integer, String> map = new HashMap<>();
+            map.put(1, "John");
+            map.put(25, "John");
+            map.put(47, "John");
+            map.put(13, "John");
+            map.put(0, "Smith");
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("Smith", 0);
+            expected.put("John", 47);
+
+            Map<String, Integer> actual = task.reverse(map);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void test4() {
+            Map<Integer, String> map = new HashMap<>();
+
+            Map<String, Integer> expected = new HashMap<>();
+            Map<String, Integer> actual = task.reverse(map);
+
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 15")
+    class Task15Test {
+
+        @Test
+        public void test1() {
+            Map<String, Integer> map = new TreeMap<>();
+            map.put("Alyssa", 22);
+            map.put("Char", 25);
+            map.put("Dan", 25);
+            map.put("Jeff", 20);
+            map.put("Kasey", 20);
+            map.put("Kim", 20);
+            map.put("Morgan", 25);
+            map.put("Ryan", 25);
+            map.put("Stef", 22);
+
+            int actual = task.rarest(map);
+
+            assertEquals(22, actual);
+        }
+
+        @Test
+        public void test2() {
+            Map<String, Integer> map = new TreeMap<>();
+            map.put("Alyssa", 22);
+            map.put("Char", 25);
+            map.put("Dan", 25);
+            map.put("Jeff", 20);
+            map.put("Kasey", 20);
+            map.put("Kim", 20);
+            map.put("Morgan", 25);
+            map.put("Ryan", 25);
+            map.put("Stef", 22);
+            map.put("Kelly", 22);
+
+            int actual = task.rarest(map);
+
+            assertEquals(20, actual);
+        }
+
+        @Test
+        public void test3() {
+            Map<String, Integer> map = new TreeMap<>();
+            map.put("Alyssa", 21);
+            map.put("Char", 21);
+            map.put("Dan", 21);
+            map.put("Jeff", 21);
+            map.put("Kasey", 21);
+            map.put("Kim", 21);
+            map.put("Morgan", 21);
+            map.put("Ryan", 21);
+            map.put("Stef", 21);
+
+            int actual = task.rarest(map);
+
+            assertEquals(21, actual);
+        }
+
+        @Test
+        public void test4() {
+            Map<String, Integer> map = new TreeMap<>();
+            map.put("Marty", 40);
+            map.put("Stuart", 50);
+
+            int actual = task.rarest(map);
+
+            assertEquals(40, actual);
+        }
+
+        @Test
+        public void test5() {
+            Map<String, Integer> map = new TreeMap<>();
+            map.put("Alyssa", 40);
+
+            int actual = task.rarest(map);
+            assertEquals(40, actual);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for the task 16")
+    class Task16Test {
+
+        @Test
+        public void test1() {
+            List<Integer> list1 = List.of(1, 1, 2, 3, 4);
+
+            int actual = task.maxOccurrences(list1);
+
+            assertEquals(2, actual);
+        }
+
+        @Test
+        public void test2() {
+            List<Integer> list1 = List.of(5, 6, 5, 6, 5, 6);
+
+            int actual = task.maxOccurrences(list1);
+
+            assertEquals(3, actual);
+        }
+
+        @Test
+        public void test3() {
+            List<Integer> list1 = List.of(0, 0, 0, 0);
+
+            int actual = task.maxOccurrences(list1);
+
+            assertEquals(4, actual);
+        }
+
+        @Test
+        public void test4() {
+            List<Integer> list1 = Collections.emptyList();
+
+            int actual = task.maxOccurrences(list1);
+
+            assertEquals(0, actual);
         }
     }
 }
